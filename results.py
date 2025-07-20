44de159ae27bad517e7f127bd88bf04fbea38c19
Gabriele <gabriele.gimelli@gmail.com>
2025-07-20 20:15:31 +0200

    .

--- results.py
+++ results.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import os
 import csv
+from matplotlib.colors import ListedColormap, BoundaryNorm
 
 def save_training_results(model_name, learning_rate, batch_size, train_loss, val_loss, miou, iou_per_class, augmentation, save_dir="results/"):
     
@@ -56,4 +57,34 @@ def plot_training_results(train_losses, val_losses, miou_scores, num_classes, sa
     plt.legend()
     plt.grid()
     plt.savefig(os.path.join(save_dir, "miou_per_epoch.png"))
+    plt.show()
+
+
+
+
+def visualize_prediction(prediction,save_dir="prediction.png"):
+
+    class_labels = ["1", "2", "3", "4","5", "6", "7", "8", "9", "10","11", "12", "13", "14", "15"]
+
+    colors = [
+        "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f",
+        "#bcbd22", "#17becf", "#aec7e8", "#ffbb78", "#98df8a", "#ff9896", "#c5b0d5"
+    ]
+
+    cmap = ListedColormap(colors[:len(class_labels)])
+    norm = BoundaryNorm(range(len(class_labels) + 1), cmap.N)
+
+
+    plt.figure(figsize=(6, 5))
+
+    pred = plt.imshow(prediction, cmap=cmap, norm=norm)
+    plt.title("Predizione")
+    plt.axis("off")
+
+    cbar = plt.colorbar(ax=plt.gcf().axes, orientation='horizontal', pad=0.5, aspect=50)
+    cbar.set_ticks(range(len(class_labels)))
+    cbar.set_ticklabels(class_labels)
+    cbar.ax.tick_params(labelsize=8)
+
+    plt.tight_layout()
     plt.show()
--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from dataset import MadosDataset
 from torch.utils.data import DataLoader
 from unet import UNet
 from sklearn.metrics import precision_score, recall_score, f1_score, jaccard_score
+from results import visualize_prediction
 
 def test_unet(model, dataloader_test):
    
@@ -23,8 +24,8 @@ def test_unet(model, dataloader_test):
             images = images.to(device)
             labels = labels.to(device)
             batch_num += 1
-            print(f"Processing batch {batch_num}/{len(dataloader_test)}")
-            # Forward
+            #print(f"Processing batch {batch_num}/{len(dataloader_test)}")
+            
             outputs = model(images)
             loss = criterion(outputs, labels)
             test_loss += loss.item()
@@ -32,7 +33,7 @@ def test_unet(model, dataloader_test):
             outputs_original = outputs  
             labels_original = labels 
 
-            # Reshape outputs e labels
+            
             outputs = outputs.permute(0, 2, 3, 1).reshape(-1, outputs.shape[1])
             labels = labels.reshape(-1)
             mask = labels != -1
@@ -44,12 +45,8 @@ def test_unet(model, dataloader_test):
             probs = torch.nn.functional.softmax(outputs, dim=1).cpu().numpy()
             preds = np.argmax(probs, axis=1)
             labels = labels.cpu().numpy()
-            #if (batch_num==49):
-            #    print(labels_original[0].cpu().numpy())
-            #    visualize_prediction(
-            #    image=images[0],  # Immagine originale
-            #    ground_truth=labels_original[0].cpu().numpy(),  # Ground truth originale
-            #    prediction=torch.argmax(outputs_original[0], dim=0).cpu().numpy())
+            if (batch_num==49):
+                visualize_prediction(prediction=torch.argmax(outputs_original[0], dim=0).cpu().numpy())
 
             y_pred += preds.tolist()
             y_true += labels.tolist()
@@ -58,7 +55,8 @@ def test_unet(model, dataloader_test):
     print(f"Test Loss: {avg_test_loss:.4f}")
     
 
-    num_classes = 15  
+
+    num_classes = 15 
     iou_per_class = jaccard_score(y_true, y_pred, average=None, labels=range(num_classes))
 
     for i, iou in enumerate(iou_per_class):
@@ -67,7 +65,7 @@ def test_unet(model, dataloader_test):
     #y_true = torch.tensor(y_true)
     #y_pred = torch.tensor(y_pred)
 
-    # metriche
+
     precision = precision_score(y_true, y_pred, average='macro', zero_division=0)
     recall = recall_score(y_true, y_pred, average='macro', zero_division=0)
     f1 = f1_score(y_true, y_pred, average='macro', zero_division=0)
@@ -85,52 +83,9 @@ def test_unet(model, dataloader_test):
 
 
 
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap, BoundaryNorm
 
-def visualize_prediction(image, ground_truth, prediction):
 
-    class_labels = [
-        "Marine Debris", "Dense Sargassum", "Sparse Floating Algae", "Natural Organic Material",
-        "Ship", "Oil Spill", "Marine Water", "Sediment-Laden Water", "Foam", "Turbid Water",
-        "Shallow Water", "Waves & Wakes", "Oil Platform", "Jellyfish", "Sea snot"
-    ]
 
-    colors = [
-        "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f",
-        "#bcbd22", "#17becf", "#aec7e8", "#ffbb78", "#98df8a", "#ff9896", "#c5b0d5"
-    ]
-
-    cmap = ListedColormap(colors[:len(class_labels)])
-    norm = BoundaryNorm(range(len(class_labels) + 1), cmap.N)
-
-    if isinstance(image, torch.Tensor):
-        image = image.permute(1, 2, 0).cpu().numpy()  
-
-    plt.figure(figsize=(15, 5))
-
-    plt.subplot(1, 3, 1)
-    plt.imshow(image.astype('uint8'))  # Assicurati che l'immagine sia in formato uint8
-    plt.title("Immagine Originale")
-    plt.axis("off")
-
-    plt.subplot(1, 3, 2)
-    gt = plt.imshow(ground_truth, cmap=cmap, norm=norm)
-    plt.title("Ground Truth")
-    plt.axis("off")
-
-    plt.subplot(1, 3, 3)
-    pred = plt.imshow(prediction, cmap=cmap, norm=norm)
-    plt.title("Predizione")
-    plt.axis("off")
-
-    cbar = plt.colorbar(gt, ax=plt.gcf().axes, orientation='horizontal', pad=0.05, aspect=50)
-    cbar.set_ticks(range(len(class_labels)))
-    cbar.set_ticklabels(class_labels)
-    cbar.ax.tick_params(labelsize=8)
-
-    plt.tight_layout()
-    plt.show()
 
 if __name__ == "__main__":
     
@@ -140,7 +95,7 @@ if __name__ == "__main__":
     
     model =  UNet(in_channels=4, out_channels=15)
 
-    model_path = r"results\Model_3\best_model.pth"
+    model_path = r"best_model.pth"
     
     model.load_state_dict(torch.load(model_path))
     print("Modello caricato.")
